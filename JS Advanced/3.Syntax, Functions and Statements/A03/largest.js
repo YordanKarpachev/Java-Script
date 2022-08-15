9337acac3cd4176627aba5cb0295b22414209786
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-08-15 04:30:29 +0200

    Add files via upload

--- JS Advanced/3.Syntax, Functions and Statements/A03/largest.js
+++ JS Advanced/3.Syntax, Functions and Statements/A03/largest.js
@@ -1,14 +1,6 @@
 function largest(a, b, c){
-    let result;
-
-    if(a >= b && a >= c){
-       result = a;
-    } else if (b >= a && b >= c){
-        result = b;
-    } else{
-        result = c;
-    }
-    console.log(`The largest number is ${result}.`)
+    
+    console.log(`The largest number is ${Math.max(a, b ,c)}.`)
 }
 
 largest(3 ,4 ,5);
