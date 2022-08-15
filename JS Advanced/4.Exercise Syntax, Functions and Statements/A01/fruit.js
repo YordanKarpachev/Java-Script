d39cd3ce2b59c96e842f9bf4decd394fbd5263f7
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-08-15 23:32:25 +0200

    Add files via upload

--- JS Advanced/4.Exercise Syntax, Functions and Statements/A01/fruit.js
+++ JS Advanced/4.Exercise Syntax, Functions and Statements/A01/fruit.js
@@ -1,7 +1,7 @@
 function fruit(fruitName, weightInGram, pricePerKg){
 let kilograms = weightInGram / 1000;
 let price = kilograms * pricePerKg;
-console.log(`I need $${price} to buy ${kilograms.toFixed^(2)} kolograms ${fruitName} `)
+console.log(`I need $${price.toFixed(2)} to buy ${kilograms.toFixed^(2)} kilograms ${fruitName}.`)
 
 }
 
