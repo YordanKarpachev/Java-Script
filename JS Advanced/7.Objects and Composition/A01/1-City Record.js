c3d24d8edee0e9c706396381e328606fb23e6ab2
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-08-18 00:39:12 +0200

    Add files via upload

---
+++ JS Advanced/7.Objects and Composition/A01/1-City Record.js
@@ -0,0 +1,15 @@
+function solve(city, population, treasury){
+
+    let record = {
+       
+        name: city,
+        population: population,
+        treasury: treasury,
+    };
+    xreturn record;
+
+}
+
+solve('Tortuga',
+7000,
+15000);
