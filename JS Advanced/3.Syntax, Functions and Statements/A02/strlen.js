e043284708b2530c4ba48b5b22ddd9ac1cfdadbe
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-08-17 04:42:23 +0200

    Add files via upload

---
+++ JS Advanced/3.Syntax, Functions and Statements/A02/strlen.js
@@ -0,0 +1,12 @@
+function strlen (a, b, c){
+    let len1 = a.length;
+    let len2 = b.length;
+    let len3 = c.length;
+
+let total = len1 + len2 + len3;
+let avg = Math.floor(total / 3);
+console.log(total);
+console.log(avg);
+
+
+}
