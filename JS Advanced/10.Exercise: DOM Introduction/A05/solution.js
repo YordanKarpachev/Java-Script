a2b35c0a3d9d179593055ea4cc732fb0e47daf22
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-08-21 01:33:52 +0200

    Add files via upload

---
+++ JS Advanced/10.Exercise: DOM Introduction/A05/solution.js
@@ -0,0 +1,19 @@
+function solve() {
+   document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let input = document.getElementById('searchField');
+   let rows = document.querySelectorAll('tbody tr');
+   
+
+   function onClick() {
+      for(let row of rows){
+
+         row.classList.remove('select');
+      }
+
+      for(let row of rows){
+         if(input.value !== '' && row.innerHTML.includes(input.value)){
+            row.className = 'select';
+         }
+      }
+   }
+}
