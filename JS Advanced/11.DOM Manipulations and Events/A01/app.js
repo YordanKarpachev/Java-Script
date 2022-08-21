544de176667905169c749ad7d63bb2daa2f44b4a
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-08-21 05:24:47 +0200

    Add files via upload

--- JS Advanced/11.DOM Manipulations and Events/A01/app.js
+++ JS Advanced/11.DOM Manipulations and Events/A01/app.js
@@ -1,3 +1,7 @@
 function addItem() {
-    console.log('TODO:...');
+    let inputElement = document.getElementById('newItemText');
+    let itemsElement = document.getElementById('items');
+    let liElement = document.createElement('li');
+    liElement.textContent = inputElement.value;
+    itemsElement.appendChild(liElement);
 }
