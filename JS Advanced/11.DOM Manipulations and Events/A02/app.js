4801c193b494dbb35a1a2b1209a425aaa1a14b7b
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-08-21 23:18:36 +0200

    Add files via upload

--- JS Advanced/11.DOM Manipulations and Events/A02/app.js
+++ JS Advanced/11.DOM Manipulations and Events/A02/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+ 
+let emailInputElement = document.querySelector('input[name="email"]');
+let emailCellElement = document.querySelectorAll('tr td:nth-of-type(2)');
+let resultElement = document.getElementById('result');
+
+let emailElements = Array.from(emailCellElement);
+let targetElement = emailElements.find(x => x.textContent == emailInputElement.value);
+
+
+if(targetElement){
+targetElement.parentNode.remove();
+resultElement.textContent = 'Deleted.'
+} else{
+    resultElement.textContent = 'Not found.'
+}
 }
