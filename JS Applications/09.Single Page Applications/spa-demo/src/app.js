08022a9f554c26aa821c2ffdc16e0d2c8c53887b
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-09 00:01:57 +0200

    Add files via upload

--- JS Applications/09.Single Page Applications/spa-demo/src/app.js
+++ JS Applications/09.Single Page Applications/spa-demo/src/app.js
@@ -1,12 +1,29 @@
-document.getElementById('homeBtn').addEventListener('click', () => {
-    document.querySelector('main').innerHTML = '<h2>Home Pate</h2><p>Welcome to our side!</p>';
-});
 
-document.getElementById('catalogBtn').addEventListener('click', () => {
-    document.querySelector('main').innerHTML = 
-    '<h2>Catalog</h2> <ul><li>Product1</li><li>Product2</li><li>Product3</li></ul > ';
-});
 
-document.getElementById('aboutBtn').addEventListener('click', () => {
-    document.querySelector('main').innerHTML = '<h2>About us</h2><p>Phone: 0176...</p>';
-});
+document.querySelector('nav').addEventListener('click', onNavigate);
+
+showView('homeView');
+
+const sections ={
+    'homeBtn' : 'homeView',
+    'catalogBtn': 'catalogView',
+    'aboutBtn': 'aboutView'
+};
+
+
+function onNavigate(event){
+    if(event.target.tagName == 'BUTTON'){
+           const sectionId = sections[event.target.id];
+            showView(sectionId);
+
+    }
+}
+
+
+function showView (id){
+    document.querySelectorAll('section').forEach(s => s.style.display ='none');
+    document.getElementById(id).style.display ='';
+}
+
+
+
