a4e952566d1fbe59693bc62289718a19c619a8b3
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-10 00:19:51 +0200

    Add files via upload

--- JS Applications/11.Architecture and Testing/test.js
+++ JS Applications/11.Architecture and Testing/test.js
@@ -1,8 +1,9 @@
 const { chromium } = require('playwright-chromium');
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto('https://google.com/');
-  await page.screenshot({ path: `example.png` });
-  await browser.close();
-})();
+const { expect } = require('chai');
+let browser, page; // Declare reusable variables
+describe('E2E tests', function() {
+  before(async () => { browser = await chromium.launch(); });
+  after(async () => { await browser.close(); });
+  beforeEach(async () => { page = await browser.newPage(); });
+  afterEach(async () => { await page.close(); }); 
+});
