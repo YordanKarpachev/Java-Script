ee9b09c2a80a785a82b6f121719483a8093c10a7
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-10 22:12:50 +0200

    Add files via upload

--- JS Applications/13.Client Side Rendering/demo/app.js
+++ JS Applications/13.Client Side Rendering/demo/app.js
@@ -1,32 +1,25 @@
 import { data, nav as links } from './data.js';
+import {createTemplate} from '/.engine.js';
 
 
 
 start();
 
-function start(){
+async function start(){
 
 const main = document.querySelector('main');
+const nav = document.querySelector('nav ul');
+
+const articleTemplateAsString = await (await fetch('./templates/article.html')).text(); 
+const articleTemplate = createTemplate(articleTemplateAsString);
 
 
 main.innerHTML = data.map(articleTemplate).join('');
 
-const nav = document.querySelector('nav ul');
+const navTemplateAsString = await (await fetch('./templates/nav.html')).text(); 
+const navTemplate = createTemplate(navTemplateAsString);
 
 nav.innerHTML = links.map(navTemplate).join('');
 
 }
 
-function articleTemplate(article){
-    return `<article>
-    <h2>${article.title}</h2>
-    <div class="content">
-        <p>${article.content}</p>
-    </div>
-    <footer>Author: ${article.author}</footer>
-    </article> `;
-}
-
-function navTemplate(nav){
-    return `<li><a href="${nav.href}">${nav.label}</a></li>`;
-}
---
+++ JS Applications/13.Client Side Rendering/demo/engine.js
@@ -0,0 +1,12 @@
+export function createTemplate(templateAsString){
+    const pattern = /{{(.+?)}}/g;
+
+
+return (data)  => templateAsString.replace(pattern, (match, name) => {
+        
+        return data[name];
+
+    });
+
+}
+
