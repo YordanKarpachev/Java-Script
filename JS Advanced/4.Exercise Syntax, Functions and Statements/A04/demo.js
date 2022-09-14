e4d7b63665ac5878b7d9c1c907ae0627810dafc6
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-14 04:16:21 +0200

    Add files via upload

--- JS Advanced/4.Exercise Syntax, Functions and Statements/A04/demo.js
+++ JS Advanced/4.Exercise Syntax, Functions and Statements/A04/demo.js
@@ -1,11 +1,11 @@
-function demo(year, month, day) {
-    let dateInput = `${year}-${month}-${day}`
-    let date = new Date(dateInput);
-    date.setDate(date.getDate()-1)
+function solve(year, month, day){
+ 
+    let currentDate = new Date(year, month - 1, day - 1);
 
-console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
+   // currentDate.setDate(currentDate.getDate() - 1);
 
-
-
-}
-demo(2016, 9, 30);
+    console.log(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1 }-${currentDate.getDate() }`);
+    }
+    
+    solve(2016, 9, 30);
+    solve(2016, 10, 1);
