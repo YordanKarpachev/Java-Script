080b9b0b90eb269fc2729ed71dff5566d7187841
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-19 04:08:27 +0200

    Add files via upload

--- JS Advanced/6.Exercise: Arrays and Nested Arrays/A06/ListOFNames.js
+++ JS Advanced/6.Exercise: Arrays and Nested Arrays/A06/ListOFNames.js
@@ -1,14 +1,7 @@
-function arraysDemo(array){
-    let sorted = array.sort((a ,b) => a.localeCompare(b));
-
-    let orderNumber = 1;
-
-    sorted.forEach((name) => {
-        console.log(`${orderNumber}.${name}`);
-        orderNumber++
-    }); 
-
+function solve(arr) {
+arr.sort((a, b) => a.localeCompare(b))
+.forEach((x, i) => console.log(`${i + 1}.${x}`));
 
 }
 
-arraysDemo(["John", "Bob", "Christina", "Ema"]);
+solve(["John", "Bob", "Christina", "Ema"])
