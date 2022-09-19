1911781fb0fd6e6ae3e3633d72b3206ac98d0d65
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-19 05:44:33 +0200

    Add files via upload

--- JS Advanced/6.Exercise: Arrays and Nested Arrays/A09/A09.js
+++ JS Advanced/6.Exercise: Arrays and Nested Arrays/A09/A09.js
@@ -1,19 +1,29 @@
-function arraysDemo(array) {
+function solve(arr) {
 
-    let sum = array[0].reduce((a, b) => a+b);
-  
- for(let i = 0; i < array[0].length ; i++){
- 
-  let currentCol = 0;
-  for(let j = 0; j < array.length; j++){
-            currentCol+=array[j][i];
+    for (let i = 0; i < arr.length - 1; i++) {
+        let sumRow = arr[i].reduce((acc, current) => acc + current);
+        let sumSecondRow = arr[i].reduce((acc, current) => acc + current);
+        let sumCol = 0;
+        let sumSecondCol = 0;
+
+        for (let j = 0; j < arr.length; j++) {
+            sumCol += arr[j][i] 
+            sumSecondCol += arr[j][i+1]   
         }
-
-        if(currentCol !== sum){
+        if(sumRow !== sumSecondRow || sumCol !== sumSecondCol){
             return false;
         }
     }
     return true;
 }
 
-arraysDemo([[4, 5, 6], [6, 5, 4], [5, 5, 5]]);
+console.log(solve([[4, 5, 6],
+    [6, 5, 4],
+    [5, 5, 5]]));
+console.log(solve([[11, 32, 45],
+    [21, 0, 1],
+    [21, 1, 1]])
+    );
+
+
+
