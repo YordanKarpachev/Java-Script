75d9ec123ccc392eb82cf768b11dba6f27c4f2c9
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-19 04:08:05 +0200

    Delete A06.js

--- JS Advanced/6.Exercise: Arrays and Nested Arrays/A06/A06.js
+++
@@ -1,8 +0,0 @@
-function solve(arr) {
-    let count = 1;
-    arr.sort((a, b) => a.localeCompare(b)).forEach(element => {
-        console.log(`${count++}.${element}`);
-    });
-}
-
-solve(["John", "Bob", "Christina", "Ema"])
