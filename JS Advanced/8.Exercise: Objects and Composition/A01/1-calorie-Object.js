e083c814749a98b2854bbf2a3420423451f8b8a0
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-22 15:05:11 +0200

    Add files via upload

--- JS Advanced/8.Exercise: Objects and Composition/A01/1-calorie-Object.js
+++ JS Advanced/8.Exercise: Objects and Composition/A01/1-calorie-Object.js
@@ -1,14 +1,11 @@
 function solve(input) {
-    let obj = {};
-   
-    for (index = 0; index < input.length; index += 2){
-        let productName = input[index];
-        let calories = input[index + 1];
-        obj[productName] = Number(calories);
+    let res = {};
 
+    for (let i = 0; i < input.length; i += 2) {
+        res[input[i]] = Number(input[i + 1]);
     }
-    console.log(obj);
-
+    console.log(res);
 }
 
-solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52'])
+solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
+solve(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42']);
