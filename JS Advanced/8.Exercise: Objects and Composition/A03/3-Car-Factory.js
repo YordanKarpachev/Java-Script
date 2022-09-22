b6771dc3a4281a79d3c34aecb435d41034c32991
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-22 16:31:23 +0200

    Add files via upload

--- JS Advanced/8.Exercise: Objects and Composition/A03/3-Car-Factory.js
+++ JS Advanced/8.Exercise: Objects and Composition/A03/3-Car-Factory.js
@@ -1,39 +1,58 @@
-function solve(obj) {
-
-    let resultCar = {};
-    let engine = {};
-    let carrige = {};
-
-    resultCar.model = obj.model;
-
-    if(obj.power <= 90) {
-        engine.power = 90;
-        engine.volume = 1800;
-
-    } else if (obj.power <= 120){
-        engine.power = 120;
-        engine.volume = 2400;
-    } else if (obj.power <= 200){
-        engine.power = 200;
-        engine.volume = 3500;
+function solve(data) {
+    let res = {};
+ 
+    res.model = data.model;
+    if (data.power <= 90) {
+        res.engine = {
+            power: 90,
+            volume: 1800
+        }
+    } else if (data.power <= 120) {
+        res.engine = {
+            power: 120,
+            volume: 2400
+        }
+    } else {
+        res.engine = {
+            power: 200,
+            volume: 3500
+        }
     }
 
-    carrige.type = obj.carriage;
-    carrige.color = obj.color;
-
-    if(obj.wheelsize % 2 === 0){
-        obj.wheelsize --;
+    if (data.carriage === 'hatchback') {
+        res.carriage = {
+            type: 'hatchback',
+            color: data.color
+        }
+    } else {
+        res.carriage = {
+            type: 'coupe',
+            color: data.color
+        }
     }
 
-    resultCar.engine = engine;
-    resultCar.carriage = carrige;
-    resultCar.wheels = [obj.wheelsize,obj.wheelsize,obj.wheelsize,obj.wheelsize]
-    return resultCar;
 
+    res.wheels = [data.wheelsize, data.wheelsize, data.wheelsize, data.wheelsize,]
+
+    if (data.wheelsize % 2 === 0) {
+        res.wheels = [data.wheelsize - 1, data.wheelsize - 1, data.wheelsize - 1, data.wheelsize - 1,]
+    }
+   return res
 }
 
-solve({ model: 'VW Golf II',
-power: 90,
-color: 'blue',
-carriage: 'hatchback',
-wheelsize: 14 })
+solve({
+    model: 'VW Golf II',
+    power: 90,
+    color: 'blue',
+    carriage: 'hatchback',
+    wheelsize: 14
+});
+console.log('----------------');
+
+solve({
+    model: 'Opel Vectra',
+    power: 110,
+    color: 'grey',
+    carriage: 'coupe',
+    wheelsize: 17
+});
