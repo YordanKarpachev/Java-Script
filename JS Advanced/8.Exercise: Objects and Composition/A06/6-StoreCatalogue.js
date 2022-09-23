979c1f63fbcaece506ffe9054c74bd50474d75e3
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-23 11:31:53 +0200

    Add files via upload

--- JS Advanced/8.Exercise: Objects and Composition/A06/6-StoreCatalogue.js
+++ JS Advanced/8.Exercise: Objects and Composition/A06/6-StoreCatalogue.js
@@ -1,28 +1,58 @@
-function solve(array) {
+function solve(input) {
+    let result = [];
 
-    let result = array.sort((a,b) => a.localeCompare(b));
+    while (input.length !== 0) {
+        let [productName, price] = input.shift().split(' : ');
 
-    let current = [];
-    let char = '';
+        let char = productName.charAt(0);
+  
+        let currentString = productName + ': ' + price;
 
-    for(let i = 0 ; i < result.length ; i++){
-        current = result[i].split(' : ');
-        if(result[i][0] !== char){
-            console.log(result[i][0]);
-            char = result[i][0];
+        if (result.filter(x => x.char === char).length > 0) {
+            let current = result.find(x => x.char === char);
+
+            current.data.push(currentString);
+        } else {
+            let obj = { char, data: [] };
+            obj.data.push(currentString);
+            result.push(obj)
+
         }
 
-    console.log(`  ${current[0]}: ${current[1]}`)
     }
+
+    result.sort((a, b) => a.char.localeCompare(b.char))
+    .forEach(a => {
+        console.log(a.char);
+        let obj = a.data;
+       //  console.log(obj.join('\n'));
+        obj.sort((a,b) => a.localeCompare(b))
+        .forEach(b => { console.log('  ' + b ) })
+
+    })
 
 
 }
 
+
+
+
 solve(['Appricot : 20.4',
-'Fridge : 1500',
-'TV : 1499',
-'Deodorant : 10',
-'Boiler : 300',
-'Apple : 1.25',
-'Anti-Bug Spray : 15',
-'T-Shirt : 10'])
+    'Fridge : 1500',
+    'TV : 1499',
+    'Deodorant : 10',
+    'Boiler : 300',
+    'Apple : 1.25',
+    'Anti-Bug Spray : 15',
+    'T-Shirt : 10']);
+
+console.log('************');
+
+solve(['Banana : 2',
+    'Rubics Cube : 5',
+    'Raspberry P : 4999',
+    'Rolex : 100000',
+    'Rollon : 10',
+    'Rali Car : 2000000',
+    'Pesho : 0.000001',
+    'Barrel : 10']);
