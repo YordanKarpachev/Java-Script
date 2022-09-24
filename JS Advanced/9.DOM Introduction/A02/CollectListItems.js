a88e37ff86fc0235cafa97bfd7d866a2e80cedb8
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-24 13:48:31 +0200

    Add files via upload

--- JS Advanced/9.DOM Introduction/A02/CollectListItems.js
+++ JS Advanced/9.DOM Introduction/A02/CollectListItems.js
@@ -1,5 +1,8 @@
 function extractText() {
+    let itemNodes = document.querySelectorAll("ul#items li");
+    let textArea = document.querySelector("#result");
+    for (let node of itemNodes) {
+        textArea.value += node.textContent + "\n";
+    }
 
-let ulElement = document.getElementById('items');
-console.log(ulElement.textContent);
 }
