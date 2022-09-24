37420c579285e82bdb8bd9b87d540d355031b56e
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-24 14:23:56 +0200

    Add files via upload

--- JS Advanced/9.DOM Introduction/A04/ShowMore.js
+++ JS Advanced/9.DOM Introduction/A04/ShowMore.js
@@ -1,6 +1,4 @@
 function showText() {
-    let textElement = document.getElementById('text');
-    textElement.style.display = 'inline';
-    let showMoreElement = document.getElementById('more');
-    showMoreElement.style.display = 'none';
+ document.getElementById("text").style.display = 'inline';
+ document.getElementById("more").style.display = 'none';
 }
