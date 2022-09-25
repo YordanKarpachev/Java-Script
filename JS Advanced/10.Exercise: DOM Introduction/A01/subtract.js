4b776189b483e06c6815e65eab48513b5b82a6ae
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-26 00:38:46 +0200

    Add files via upload

--- JS Advanced/10.Exercise: DOM Introduction/A01/subtract.js
+++ JS Advanced/10.Exercise: DOM Introduction/A01/subtract.js
@@ -1,15 +1,3 @@
 function subtract() {
-    let firstNumber = document.getElementById('firstNumber').value;
-    let firstNumAsNumber = Number(firstNumber);
-
-    let secondNumber = document.getElementById('secondNumber').value;
-    let secondNumAsNumber = Number(secondNumber);
-
-    let result = firstNumAsNumber - secondNumAsNumber;
-    let resultElement = document.getElementById('result');
-    resultElement.textContent = result;
-    console.log(result);
-
-
-
+    console.log('TODO:...');
 }
