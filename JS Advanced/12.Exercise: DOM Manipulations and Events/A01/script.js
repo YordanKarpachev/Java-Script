1d4ccce1adf1cc89d5ae5e2604ff95ff479caf97
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-09-29 01:32:04 +0200

    Add files via upload

--- JS Advanced/12.Exercise: DOM Manipulations and Events/A01/script.js
+++ JS Advanced/12.Exercise: DOM Manipulations and Events/A01/script.js
@@ -1,24 +1,30 @@
 function create(words) {
-   let parentElement = document.getElementById('content');
-   let elements = words;
 
-   for (let i = 0; i < elements.length; i++) {
+   for (let word of words) {
 
       let div = document.createElement('div');
       let p = document.createElement('p');
-      let text = document.createTextNode(elements[i]);
+      p.textContent = word;
 
-      p.appendChild(text);
       p.style.display = 'none';
-      div.appendChild(p);
 
-      div.addEventListener('click', showParagraphs);
-      parentElement.appendChild(div);
+      div.appendChild(p);
+      div.addEventListener('click', function (event) {
+
+         if(event.target.nodeName ==='P'){
+            return
+         }
+      
+         event.target.children[0].style.display = 'block';
+      
+      })
+      
+      
+      
+      
+      document.getElementById('content').appendChild(div);
    }
-   function showParagraphs(event) {
-      event.target.children[0].style.display = 'inline';
-   }
+
 
 
 }
-
