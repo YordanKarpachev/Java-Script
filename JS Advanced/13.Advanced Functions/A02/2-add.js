203f849005b26f80484afe102f68a18703725689
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-10-01 21:48:04 +0200

    Add files via upload

--- JS Advanced/13.Advanced Functions/A02/2-add.js
+++ JS Advanced/13.Advanced Functions/A02/2-add.js
@@ -1,10 +1,10 @@
-function solution(number){
-
-    function add (a,b){
-        return a +b;
+function solution(num) {
+    function add(a, b) {
+        return a + b;
     }
 
-    return add.bind(this, number);
+    return add.bind(this, num)
+
 }
 
 
