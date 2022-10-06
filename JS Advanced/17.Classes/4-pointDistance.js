e14ba3cd522dccb416932e3444288b5c531bd6d5
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-10-07 00:22:21 +0200

    Add files via upload

--- JS Advanced/17.Classes/4-pointDistance.js
+++ JS Advanced/17.Classes/4-pointDistance.js
@@ -1,14 +1,18 @@
-class Point{
-    constructor(x, y){
+class Point {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
+
     }
 
-    static distance(firstPoint, secondPoint){
-        let a = Math.abs(firstPoint.x - secondPoint.x);
-        let b = Math.abs(firstPoint.y - secondPoint.y);
-        let distance = Math.sqrt( a** 2 + b **2);
+    static distance = (first, second) => {
+        let dx = second.x - first.x;
+        let dy = second.y - first.y;
+        dx **= 2;
+        dy **= 2;
+        return Math.sqrt(dx + dy);
 
-        return distance;
     }
+
+
 }
