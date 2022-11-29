12ef926ac71b5551a84bef644cbd5547ad3c72df
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-11-29 22:39:00 +0100

    Add files via upload

---
+++ src/util.js
@@ -0,0 +1,29 @@
+
+
+export const getUserData = () => {
+const data = JSON.parse(sessionStorage.getItem("userData"));
+return data;
+
+}
+
+export const setUserData = (data) => {
+sessionStorage.setItem("userData", JSON.stringify(data));
+}
+
+
+export const  clearUserData = () => {
+    sessionStorage.removeItem("userData");
+
+}
+
+export const createSubmitHandler = (callback) => {
+
+    return function (event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData);
+        callback(data);
+    }
+
+
+}
