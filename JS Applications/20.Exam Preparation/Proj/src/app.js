409a52bae1389d5b5fef1f511b5efb13ce9aafbc
Yordan <97124750+YordanKarpachev@users.noreply.github.com>
2022-11-29 22:36:38 +0100

    Add files via upload

---
+++ JS Applications/20.Exam Preparation/Proj/src/app.js
@@ -0,0 +1,46 @@
+import {page, render} from "./lib.js"
+import { getUserData } from "./util.js";
+import { catalogView } from "./views/catalogView.js";
+import { createView } from "./views/createView.js";
+import { homeView } from "./views/homeView.js";
+import { loginView } from "./views/loginView.js";
+import { registerView } from "./views/registerView.js";
+import {updateNav} from "./views/nav.js"
+import { showDetails } from "./views/detailsView.js";
+
+const main = document.getElementById( "main-content");
+
+
+
+page(middleware);
+page("/", homeView);
+page("/home", homeView);
+page("/catalog", catalogView);
+page("/login", loginView);
+page("/register", registerView);
+page("/create", createView);
+page("/details/:id",showDetails);
+//page("/edit:id", console.log("EDIT ID"));
+
+page.start();
+
+
+
+
+function middleware(ctx, next) {
+    ctx.render = myRender;
+    ctx.updateNav = updateNav;
+    updateNav();
+    const user = getUserData();
+    if(user) {
+        ctx.user = user;
+    }
+    next();
+}
+
+
+function myRender(content) {
+
+    render(content, main);
+    
+}
